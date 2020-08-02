287e9cecb34a24a848abfabb7035d77df65c1041
Andrei <andrei.matei@bawagpsk.com>
2020-08-02 22:46:33 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const promBundle = require("express-prom-bundle");
+
+const express = require('express');
+
+const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
+
+let corsOptions = {
+    exposedHeaders: ['Accept-Language',
+        'Access-Control-Allow-Origin',
+       'Connection', 'Content-Length', 'Content-Type', 'Date',
+        'Etag', 'Server', 'Via', 'X-Powered-By']
+};
+
+
+const metricsMiddleware = promBundle({includeMethod: true});
+
+const PORT = 3000;
+
+var appcore = express();
+appcore.use(metricsMiddleware);
+appcore.use(cors(corsOptions));
+
+appcore.get('/', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(
+        {
+            id: uuidv4(),
+            name: 'Welcome to Core-app-1'
+        }));
+});
+
+appcore.listen(PORT, () => {
+    console.log('Core-Service-1 app started on port 3000')
+});
