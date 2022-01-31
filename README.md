65d546560d1403e7f139f77a4048df9180bc1413
Andrei <andrei@devopslog.eu>
2022-01-31 23:29:40 +0100

    Update app to latest versions

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# doxi-uuid
+# deli
 Small NodeJS/Express app that returns an UUID, static name and hostname as a JSON.
 Written to test Kubernetes pods/services/ingress as a Node microservice with an exposed API.
-
-You can find the container image here: https://hub.docker.com/repository/docker/andreimatei/doxi-uuid 
--- index.js
+++ index.js
@@ -1,27 +1,63 @@
 const os = require("os");
-
 const express = require('express');
-
-const { v4: uuidv4 } = require('uuid');
+var appcore = express();
+const {
+    v4: uuidv4
+} = require('uuid');
 
 const PORT = 3000;
 var hostname = os.hostname();
 
-var appcore = express();
-appcore.use(express.urlencoded({ extended: true }))
+appcore.use(express.urlencoded({
+    extended: true
+}))
+
+function generateUuid() {
+    return uuidv4();
+}
 
 appcore.get('/', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    id = uuidv4()
-    res.send(JSON.stringify(
-        {
-            id: id,
-            name: 'Welcome to Core-app-1',
-            hostname: hostname
-        }));
-        console.log(id,req.hostname,"-",req.ip,"-",req.method,":",req.body.testBody);
+    id = generateUuid()
+    res.send(JSON.stringify({
+        id: id,
+        name: 'Test app',
+        hostname: hostname
+    }));
+    console.log(id, req.method, req.hostname, req.ip);
+});
+
+appcore.get('/uuid', (req, res) => {
+    id = generateUuid()
+    console.log(id, req.method, req.hostname, req.ip);
+    return res.send(`Received a GET HTTP method ${id}`);
+});
+
+appcore.post('/uuid', (req, res) => {
+    id = generateUuid()
+    console.log(id, req.method, req.hostname, req.ip);
+    return res.send(`Received a POST HTTP method ${id}`);
+});
+
+appcore.put('/uuid', (req, res) => {
+    id = generateUuid()
+    console.log(id, req.method, req.hostname, req.ip);
+    return res.send(`Received a PUT HTTP method ${id}`);
+});
+
+appcore.delete('/uuid', (req, res) => {
+    id = generateUuid()
+    console.log(id, req.method, req.hostname, req.ip);
+    return res.send(`Received a DELETE HTTP method ${id}`);
+});
+
+appcore.get('/health', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({
+        status: 'OK',
+    }));
 });
 
 appcore.listen(PORT, () => {
-    console.log('Core-Service-1 app started on port 3000')
+    console.log('Application started on port 3000')
 });
